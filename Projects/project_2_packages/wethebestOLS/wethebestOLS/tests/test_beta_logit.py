f7c3d38dd57bcd31317dd7c64fdaa43ad0bba9d1
Franco Calle <francocalle93@gmail.com>
2019-12-07 16:39:34 -0500

    updat

---
+++ Projects/project_2_packages/wethebestOLS/wethebestOLS/tests/test_beta_logit.py
@@ -0,0 +1,32 @@
+import unittest
+import numpy as np
+import sys
+import os
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+sys.path.append(os.path.abspath('..'))
+from logit import optimization
+from logit import objectiveFunction
+
+class test_beta_logit( unittest.TestCase ) :
+
+    def test_beta(self):
+
+        # Generate fake data
+        nObs = 10000
+        nVars = 4
+
+        bbeta = np.array([1, 3, 5, 7])
+        X = np.random.random((nObs,4))
+        Y = (np.random.uniform(0,1,nObs) < 0.5).astype(int).reshape(nObs,1)
+        clf = LogisticRegression(random_state=0).fit(X, Y)
+        beta_1 = clf.coef_
+
+        parameters = optimization(objectiveFunction, Y, X, np.array([1,2,3,4]))
+
+        beta_2 = parameters['x']
+
+        # Find test beta_hat is close to beta
+        tol = .01
+        abs_diff = np.all(abs(beta_1 - beta_2) < .05)
+        self.assertTrue(abs_diff)
